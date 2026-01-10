392fe8994222cee5438ad8bd4ae885b2e0706a70
dirkio <hallo@dirkio.xyz>
2026-01-10 17:03:11 +0100

    first commit

---
+++ app/components/app/AppHeader.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { useDocusI18n } from '../../composables/useDocusI18n'
+
+const appConfig = useAppConfig()
+const site = useSiteConfig()
+
+const { localePath, isEnabled, locales } = useDocusI18n()
+
+const links = computed(() => appConfig.github && appConfig.github.url
+  ? [
+      {
+        'icon': 'i-simple-icons-github',
+        'to': appConfig.github.url,
+        'target': '_blank',
+        'aria-label': 'GitHub',
+      },
+    ]
+  : [])
+</script>
+
+<template>
+  <UHeader
+    :ui="{ center: 'flex-1' }"
+    :to="localePath('/')"
+    :title="appConfig.header?.title || site.name"
+  >
+
+    <template #title>
+      <AppHeaderLogo class="lg:pl-6.5 h-5 font-medium tracking-widest text-xs w-auto shrink-0" />
+    </template>
+
+    <template #right>
+      <AppHeaderCTA />
+
+      <template v-if="isEnabled && locales.length > 1">
+        <ClientOnly>
+          <LanguageSelect />
+
+          <template #fallback>
+            <div class="h-8 w-8 rounded-full animate-pulse bg-neutral-200 dark:bg-neutral-800" />
+          </template>
+        </ClientOnly>
+
+        <USeparator
+          orientation="vertical"
+          class="h-8"
+        />
+      </template>
+
+
+      <ClientOnly>
+        <UColorModeButton class="rounded-full" />
+
+        <template #fallback>
+          <div class="h-8 w-8 animate-pulse bg-neutral-200 dark:bg-neutral-800 rounded-full" />
+        </template>
+      </ClientOnly>
+
+      <template v-if="links?.length">
+        <UButton
+          v-for="(link, index) of links"
+          :key="index"
+          v-bind="{ color: 'neutral', variant: 'ghost', ...link }"
+        />
+      </template>
+    </template>
+
+    <template #toggle="{ open, toggle }">
+      <IconMenuToggle
+        :open="open"
+        class="lg:hidden"
+        @click="toggle"
+      />
+    </template>
+
+    <template #body>
+      <AppHeaderBody />
+    </template>
+  </UHeader>
+</template>
