5b789d2d45cd0248a9b6a724402e4279efcac075
Dirk Neumann <hallo@dirkio.xyz>
2026-01-11 16:59:13 +0100

    Update default.vue (welcome message)

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="mx-auto text-center pt-40 items-center justify-center">
   <h1 class="pb-20">Hallo. Wilkommen.</h1>
+<div class="max-w-3xl mx-auto">
+  <p>My site is undergoing a few changes. <br/> Please check again soon.</p>
+    <div/>
   <ChidiAvatar />
   </div>
 </template>
