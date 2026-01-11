ca5b156fb9285e1674c8332a9f7d6001652a2f2e
dirkio <hallo@dirkio.xyz>
2026-01-11 23:44:33 +0100

    misc edits

--- app/app.config.ts
+++ app/app.config.ts
@@ -7,7 +7,6 @@ export default defineAppConfig({
   },
 
   ui: {
-    
     contentToc: {
       slots: {
         root: "sticky top-(--ui-header-height) z-10 bg-default/75 lg:bg-[initial] backdrop-blur -mx-4 px-4 sm:px-6 sm:-mx-6 overflow-y-auto max-h-[calc(100vh-var(--ui-header-height))]",
