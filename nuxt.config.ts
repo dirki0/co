c6646768649790169ec26047ee4fe7e56ef7dab5
dirkio <hallo@dirkio.xyz>
2026-01-12 06:37:19 +0100

    site title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 import { siteConfig } from './config/siteConfig'
 
 export default defineNuxtConfig({
+  site: {
+    name: 'chidi odili',
+  },
   css: ['~/assets/css/main.css'],
   modules: ['@nuxtjs/i18n', 'motion-v/nuxt'],
   i18n: {
