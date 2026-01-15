dc706d70e748464dfae91c5ccdbbd4237107a08c
dirkio <hallo@dirkio.xyz>
2026-01-15 05:19:20 +0100

    hide GIF on mobile

--- app/layouts/landingpage.vue
+++ app/layouts/landingpage.vue
@@ -2,7 +2,7 @@
 <template>
   <UMain>
     <slot />
-    <div class="mx-auto h-screen overflow-hidden fixed -z-[100] inset-0">
+    <div class="mx-auto h-screen overflow-hidden fixed -z-[100] hidden md:block inset-0">
     <img src="/images/1984.gif" width="2000px" height="2000px" class="aspect-1/1 h-screen object-cover rounded-0">
   </div>
   </UMain>
